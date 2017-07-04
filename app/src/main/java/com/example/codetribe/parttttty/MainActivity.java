271e03485ef58cdc7fdf72ad500d010534063bec
Emelda <phashae25@gmail.com>
2017-07-04 10:24:35 +0200

    Initial commit

---
+++ app/src/main/java/com/example/codetribe/parttttty/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.codetribe.parttttty;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+
+}
